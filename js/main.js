1f8e0bff5652aa50b4d9a346c633f1dcbba8c6e7
Diwas Poudel <n_diwas.poudel@cashdirector.com>
2022-07-10 18:53:52 +0545

    new changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   var event = $('.eventsides');
   var preslides = $('.preeventslides');
   var footerslides = $('.footerslides');
+  var card = $('.cardslider');
   owl.owlCarousel({
     // items change number for slider display on desktop
     loop: true,
@@ -37,6 +38,31 @@ $(document).ready(function () {
     nav: true,
     smartSpeed: 1000,
     margin: 15,
+  });
+  card.owlCarousel({
+    // items change number for slider display on desktop
+    loop: true,
+    autoplay: false,
+    dots: false,
+    stagePadding: 22,
+    nav: true,
+    smartSpeed: 1000,
+    margin: 30,
+    navText: [
+      "<div class='nav-btn prev-slide'><i class='bi bi-chevron-left'></i></div>",
+      "<div class='nav-btn next-slide'><i class='bi bi-chevron-right'></i></div>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
   footerslides.owlCarousel({
     items: 1,
